5eb734e380891fb0ed6bf83b6ffbcfff4e324046
Angie Argueta <telmabrown@Angies-MBP.fios-router.home>
2020-11-08 20:58:00 -0500

    added html and js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Variables for the main section
+var quizContentEl = document.querySelector("#quiz-content");
+var questionCount = 0;
+var contentHeading = document.getElementById("content-heading");
+var contentArea = document.getElementById("content-area");
+var counter = 75;
+var countDownEl = document.getElementById("count");
+var correctOrWrong = "";
+var scores = [];
+

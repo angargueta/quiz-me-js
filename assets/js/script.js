8631e1d2e56401f76bca22f705c6ae10196a668b
Angie Argueta <telmabrown@Angies-MBP.fios-router.home>
2020-11-08 21:08:05 -0500

    functions to begin quiz and questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,3 +38,68 @@ var questions = [
         { question: "A very useful tool used during development and debugging for printing content to the debugger is:", a1: "javascript", a2: "terminal/bash", a3: "for loops", a4: "console.log", correctAnswer: "for loops" },
     
 ];
+
+// function to start the quiz
+var startQuiz = function() {
+    questionCount = 0;
+    counter = 75;
+    contentHeading.innerText = "Coding Quiz Challenge";
+    contentArea.innerHTML = "<p>Try to answer the following code-related questions within the time limit.<br> Keep in mind that incorrect answers will penalize your score/time by ten seconds!</p></br><button id='start-button'>Start Quiz!</button>";
+    document.getElementById("start-button").addEventListener("click", presentQuestion);
+    document.getElementById("start-button").addEventListener("click", startTimer);
+    return;
+};
+
+// function to handle presentation of questions
+var presentQuestion = function() {
+    contentHeading.innerText = questions[questionCount].question;
+    contentArea.innerHTML = 
+        "<button id='answer-button1'>" + questions[questionCount].a1 + 
+        "</button></br><button id='answer-button2'>" + questions[questionCount].a2 + 
+        "</button></br><button id='answer-button3'>" + questions[questionCount].a3 +
+        "</button></br><button id='answer-button4'>" + questions[questionCount].a4 + 
+        "</button>";
+
+    var feedback = document.createElement("h2");
+    feedback.textContent = correctOrWrong;
+    contentArea.appendChild(feedback);
+
+    document.getElementById("answer-button1").addEventListener("click", confirmAnswer);
+    document.getElementById("answer-button2").addEventListener("click", confirmAnswer);
+    document.getElementById("answer-button3").addEventListener("click", confirmAnswer);
+    document.getElementById("answer-button4").addEventListener("click", confirmAnswer);
+};
+
+
+
+var confirmAnswer = function(event) {
+    
+    var targetEl = event.target;
+    // Conditional statement to check if answer is correct
+    if (targetEl.textContent === questions[questionCount].correctAnswer) {
+        correctOrWrong = "Correct!"
+    } else {
+        correctOrWrong = "Wrong!";
+        counter -= 10;
+    }
+    questionCount++;
+    return presentQuestion();
+};
+
+
+var allDone = function() {
+    
+    contentHeading.innerText = "All Done!";
+    contentArea.innerHTML = "<p>Your final score is " + counter + "</p><br/>";
+    var enterHighScore = document.createElement("input");
+
+    enterHighScore.name = "user-initials";
+  
+    contentArea.appendChild(enterHighScore);
+
+    var submitButton = document.createElement("button");
+    submitButton.textContent = "Submit";
+    submitButton.id = "submit-button";
+    contentArea.appendChild(submitButton);
+    document.getElementById("submit-button").addEventListener("click", handleSubmit);
+};
